610dfff45b68cc60360697ca259eeeb0b50420e1
rifandani <ipandani2505@gmail.com>
2020-12-21 23:38:18 +0700

    added /postal GET route

--- index.js
+++ index.js
@@ -7,10 +7,32 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
+// all postals
 app.get('/postals', function (req, res) {
   const postalArr = postals;
 
   res.status(200).json(postalArr);
+});
+
+// one postal with query 'code'
+// https://roompy-postals.herokuapp.com/postal?code=76111
+app.get('/postal', function (req, res) {
+  const code = req.query.code;
+  const postalArr = postals;
+
+  const postal = postalArr.filter((el) => el.postal_code === code.toString());
+
+  if (postal === []) {
+    return res
+      .status(200)
+      .json({
+        postal,
+        msg: 'Please, input a valid Indonesian Postal Code',
+        code,
+      });
+  }
+
+  res.status(200).json(postal);
 });
 
 app.listen(PORT, function () {
