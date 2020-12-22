cd92b17fd0578685547b62b87eedf8f07972225d
rifandani <ipandani2505@gmail.com>
2020-12-22 12:12:31 +0700

    support lurah, camat, kota query

--- index.js
+++ index.js
@@ -17,26 +17,60 @@ app.get('/postals', function (req, res) {
 // one postal with query 'code'
 // https://roompy-postals.herokuapp.com/postal?code=76111
 app.get('/postal', function (req, res) {
+  const lurah = req.query.lurah;
+  const camat = req.query.camat;
+  const kota = req.query.kota;
   const code = req.query.code;
-  if (!code)
-    return (
-      res.status(400), json({ error: true, msg: 'Please input a "code" query' })
+  const postalArr = postals;
+  let result = [];
+
+  // jika ada query lurah
+  if (lurah) {
+    const urbans = postalArr.filter((el) =>
+      el.urban.includes(lurah.toUpperCase()),
     );
 
-  const postalArr = postals;
-  const postal = postalArr.filter((el) => el.postal_code === code.toString());
-
-  if (postal === []) {
-    return res.status(200).json({
-      postal,
-      msg: 'Please, input a valid Indonesian Postal Code',
-      code,
-    });
+    for (let i = 0; i < urbans.length; i++) {
+      result.push(urbans[i]);
+    }
   }
 
-  res.status(200).json(postal);
+  // kalau ada query camat
+  if (camat) {
+    const sub_districts = postalArr.filter((el) =>
+      el.sub_district.includes(camat.toUpperCase()),
+    );
+
+    for (let i = 0; i < sub_districts.length; i++) {
+      result.push(sub_districts[i]);
+    }
+  }
+
+  // kalau ada query kota
+  if (kota) {
+    const cities = postalArr.filter((el) =>
+      el.city.includes(kota.toUpperCase()),
+    );
+
+    for (let i = 0; i < cities.length; i++) {
+      result.push(cities[i]);
+    }
+  }
+
+  // kalau ada query code
+  if (code) {
+    const postal_codes = postalArr.filter(
+      (el) => el.postal_code === code.toString(),
+    );
+
+    for (let i = 0; i < postal_codes.length; i++) {
+      result.push(postal_codes[i]);
+    }
+  }
+
+  res.status(200).json(result);
 });
 
 app.listen(PORT, function () {
-  console.log('CORS-enabled web server listening on port 80');
+  console.log(`CORS-enabled web server listening on port ${PORT}`);
 });
