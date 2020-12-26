07ebdb9f353dbb33589ac1ba34d1b68813de041f
rifandani <ipandani2505@gmail.com>
2020-12-26 19:04:38 +0700

    TRYING fixing

--- functions/app/index.js
+++ functions/app/index.js
@@ -1,19 +1,40 @@
 const express = require('express');
+const cors = require('cors');
 // files
-const router = require('../../router');
 
-/* My express App */
 function expressApp(functionName) {
+  // init express app and Router
   const app = express();
+  const router = express.Router();
 
   // Set router base path if in local dev
   const routerBasePath =
     process.env.NODE_ENV === 'dev'
       ? `/${functionName}`
       : `/.netlify/functions/${functionName}/`;
+  // : `/.netlify/functions/${functionName}/`;
+
+  // routes
+  router.get('/users', (req, res) => {
+    res.json({
+      users: [
+        {
+          name: 'steve',
+        },
+        {
+          name: 'joe',
+        },
+      ],
+    });
+  });
 
   // Setup routes
   app.use(routerBasePath, router);
+
+  // Apply express middlewares
+  router.use(cors());
+  router.use(express.json());
+  router.use(express.urlencoded({ extended: true }));
 
   return app;
 }
